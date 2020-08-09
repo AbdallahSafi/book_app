2779acd862515106d306456f4d47197ed08a17a8
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-08-09 18:12:01 +0300

    make an error view

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 // Decalaring varaibles
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 // initialize the server
 const app = express();
@@ -12,73 +12,87 @@ const app = express();
 app.use(cors());
 
 // Use super agent
-const superagent = require('superagent');
+const superagent = require("superagent");
 
 // Declare a port
 const PORT = process.env.PORT || 3000;
 
 // Test the server
 app.listen(PORT, () => {
-  console.log('I am listening to port: ', PORT);
+  console.log("I am listening to port: ", PORT);
 });
 
 // view engine setup
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //setup public folder
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 //set the encode for post body request
 app.use(express.urlencoded({ extended: true }));
 
 // test routes
-app.get('/', (req, res) => {
-  res.render('pages/index');
+app.get("/", (req, res) => {
+  res.render("pages/index");
 });
 
 // New search route
-app.get('/searches/new', (req, res) => {
-  res.render('pages/searches/new');
+app.get("/searches/new", (req, res) => {
+  res.render("pages/searches/new");
 });
 
 //Handle sreach request
-app.post('/searches', async (req, res) => {
+app.post("/searches", async (req, res) => {
   let searchInput = req.body.searchInput;
   let searchType = req.body.searchType;
-  let status = 200;
-  let booksList = await getBooks(searchInput, searchType);
-  // res.status(status).send();
+  let result = await getBooks(searchInput, searchType);
+  if(result.status === 200){
   res.render('pages/searches/show',{
-    books:booksList
-    })
+    books:result.booksList
+    });
+  }else{
+    res.render('pages/error',{
+      error:result
+      });
+  }
+
 });
 
 // fucntion to get books from google book api
 function getBooks(searchInput, searchType) {
-  let url = 'https://www.googleapis.com/books/v1/volumes';
+  let url = "https://www.googleapis.com/books/v1/volumes";
   let queryParams = {
-      q: `in${searchType}:${searchInput}`,
-    };
-  let data = superagent
+    q: `in${searchType}:${searchInput}`,
+  };
+  let result = superagent
     .get(url)
     .query(queryParams)
     .then((res) => {
-      return res.body.items.map(e =>{
+
+      let booksList = res.body.items.map((e) => {
         return new Book(e);
       });
+      return {
+        status: res.status,
+        booksList: booksList
+      }
     })
     .catch((error) => {
-      console.log(error);
+      return {
+        status: error.status,
+        message: error.response.text,
+      };
     });
-  return data;
+  return result;
 }
 
 // Render data in show
 
 // creating book constructor
 function Book(data) {
-  this.image = data.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpg';
+  this.image =
+    data.volumeInfo.imageLinks.thumbnail || "https://i.imgur.com/J5LVHEL.jpg";
   this.title = data.volumeInfo.title;
   this.authers = data.volumeInfo.authors;
-  this.description = data.volumeInfo.description || 'There is no description';
+  this.description = data.volumeInfo.description || "There is no description";
 }
